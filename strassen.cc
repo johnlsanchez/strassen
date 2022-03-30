105b9941df4a8628ecb5a85e81b50bffa2972fa2
Angel Mata <angelmat@mit.edu>
2022-03-30 16:33:41 -0400

    slight reformat

--- strassen.cc
+++ strassen.cc
@@ -6,62 +6,33 @@
 #include <ctime>
 #include <iostream>
 
-// struct matrix {
-//     int* mat;
-
-//     matrix(int n, int* values) {
-//         mat = new int[n * n];
-//         for (int i = 0; i < n * n; i++) {
-//             mat[i] = values[i];
-//         }
-//     }
-// };
-
-// // matrix* transpose(int n, matrix* a) {
-// //     int ans[n * n];
-// //     if ()
-// // }
-
-// matrix* standard_mult(int n, matrix* a, matrix* b) {
-//     int answer[n * n];
-//     matrix* b_t = b; // change to transpose
-
-//     int pos = 0;
-//     for (int i = 0; i < n; i++) {
-//         int sum = 0;
-//         for (int j = 0; j < n * n; j++) {
-//             sum += a->mat[i * n + j % n] * b_t->mat[j];
-//             if (j % n == n - 1) {
-//                 answer[pos] = sum;
-//                 pos++;
-//                 sum = 0;
-//             }
-
-//         }
-//     }
-
-
-//     matrix* product = new matrix(n, answer);
-//     return product;
-// }
-
 class matrix {
 public:
     matrix(int* file_data, int dim) {
-        data_ = (int*)malloc(sizeof(int) * dim * dim);
+        data_ = file_data;
         dim_ = dim;
-        for (int i = 0; i < dim_ * dim_; i++) {
-            data_[i] = file_data[i];
-        }
     }
 
     void print() {
         for (int i = 0; i < dim_; i++) {
             for (int j = 0; j < dim_; j++) {
-                std::cout << data_[i * dim_ + j] << ' ';
+                std::cout << (*this)(i,j) << ' ';
             }
             std::cout << '\n';
         }
+        std::cout << '\n';
+    }
+
+    inline int& operator()(int r, int c) {
+        return data_[dim_ * r + c];
+    }
+
+    inline int operator()(int r, int c) const {
+        return data_[dim_ * r + c];
+    }
+
+    inline int dim() {
+        return dim_;
     }
 
     matrix mul(matrix B) {
@@ -71,7 +42,7 @@ public:
             for (int j = 0; j < this->dim_; j++) {
                 int sum = 0;
                 for (int k = 0; k < this->dim_; k++) {
-                    sum += this->at(j, k) * B.at(k, i);
+                    sum += (*this)(j, k) * B(k, i);
                 }
                 c_data[j * this->dim_ + i] = sum;
             }
@@ -79,66 +50,31 @@ public:
         return matrix(c_data, this->dim_);
 
     }
-    matrix add(matrix B) {
+
+    matrix operator+(matrix B) {
         int* c_data;
         c_data = (int*)malloc(sizeof(int) * this->dim_ * this->dim_);
         for (int i = 0; i < this->dim_ * this->dim_; i++) {
-            c_data[i] = this->at(i / this->dim_, i % this->dim_) + B.at(i / this->dim_, i % this->dim_);
+            c_data[i] = (*this)(i / this->dim_, i % this->dim_) + B(i / this->dim_, i % this->dim_);
         }
         return matrix(c_data, this->dim_);
 
     }
-    matrix sub(matrix B) {
+
+    matrix operator-(matrix B) {
         int* c_data;
         c_data = (int*)malloc(sizeof(int) * this->dim_ * this->dim_);
         for (int i = 0; i < this->dim_ * this->dim_; i++) {
-            c_data[i] = this->at(i / this->dim_, i % this->dim_) - B.at(i / this->dim_, i % this->dim_);
+            c_data[i] = (*this)(i / this->dim_, i % this->dim_) - B(i / this->dim_, i % this->dim_);
         }
         return matrix(c_data, this->dim_);
-
-    }
-    
-
-    matrix strassen(matrix B) {
-        
-
     }
 
-    int at(int r, int c) {
-        return data_[r * this->dim_ + c];
-    }
-
-    int block_at(int r, int c, int quad_number, int block_size) {
-        int child_start_r = r + block_size * (quad_number % 2);
-    }
 
 private:
     int* data_;
     int dim_;
-    int* strassen_rec(int* data) {
-        return NULL;
-    };
 };
-
-//void make_matrix_h(int& in_data, int& data, int big_dim, int dim, int seg, int r, int c) {
-//    if (dim == 2) {
-//        data[r * big_dim + c] = in_data[r * big_dim + c];
-//        data[r * big_dim + c] = in_data[r * big_dim + c + 1];
-//        data[r * big_dim + c] = in_data[r * big_dim + c];
-//        data[r * big_dim + c] = in_data[r * big_dim + c];
-//    }
-//}
-//
-//int* make_matrix(int* in_data, int dim) {
-//    int* data;
-//    data = (int*)malloc(sizeof(int) * dim * dim);
-//    make_matrix_h(*in_data, *data, dim, dim / 2, 0, 0, 0);
-//    make_matrix_h(*in_data, *data, dim, dim / 2, 1, 0, dim / 2);
-//    make_matrix_h(*in_data, *data, dim, dim / 2, 2, dim / 2, 0);
-//    make_matrix_h(*in_data, *data, dim, dim / 2, 3, dim / 2, dim / 2);
-//}
-
-
 
 int main() {
     int a[4] = { 1, 2, 3, 4 };
@@ -147,6 +83,6 @@ int main() {
     matrix B = matrix(&b[0], 2);
     A.print();
     B.print();
-    matrix C = A.sub(B);
+    matrix C = A - B;
     C.print();
 ;}
